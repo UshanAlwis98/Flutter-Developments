fd6bb35605c92fae2e6bca84f5eee6a3e17fac8c
ushanAlwis98 <ushanloshitha@gmail.com>
2026-02-10 20:26:07 +0530

    More Widgets added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main(){
-
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.deepPurple,
+                Colors.purple,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              ),
+          ),
+          child: Center(
+            
+            child: Text(
+               style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 24
+              ),
+              'Hello World'),
+            ),
+            ),
+      ),
+    ),
+  );
 }
-
-runApp();
